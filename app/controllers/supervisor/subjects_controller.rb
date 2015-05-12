715a634b31f158deb44df4c4584b2bc7cc6bb28a
Lim Kimhuor <kimhuorlim@gmail.com>
2015-05-12 23:26:43 +0700

    Create_subject_tasks

--- app/controllers/supervisor/subjects_controller.rb
+++ app/controllers/supervisor/subjects_controller.rb
@@ -2,4 +2,21 @@ class Supervisor::SubjectsController < ApplicationController
   def new
     @subject = Subject.new
   end
+
+  def create
+    @subject = Subject.new subject_params
+    if @subject.save
+      flash[:success] = t(:create_succes, model: :"Subject")
+      redirect_to root_url
+    else
+      render "new"
+    end   
+  end
+
+  private
+  def subject_params
+    params.require(:subject).permit :name, :description, :status , 
+                                    tasks_attributes: [:id, :name, :description, 
+                                                      :status, :_destroy]
+  end
 end
