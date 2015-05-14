b8a1f8fda375936871111830604d22d0d892ebd8
Phorn Sopheak <phornsopheak17@gmail.com>
2015-05-14 13:52:01 +0700

    Show Course

--- app/controllers/supervisor/courses_controller.rb
+++ app/controllers/supervisor/courses_controller.rb
@@ -7,7 +7,7 @@ class Supervisor::CoursesController < ApplicationController
     @course = Course.new course_params
     if @course.save
       flash[:info] = t(:create_succes, model: :"Course")
-      redirect_to root_url
+      redirect_to [:supervisor, @course]
     else
       render "new"
     end
@@ -21,10 +21,14 @@ class Supervisor::CoursesController < ApplicationController
     @course = Course.find params[:id]
     if @course.update_attributes(course_params)
       flash[:info] = t(:edit_succes, model: :"Course")
-      redirect_to root_url
+      redirect_to [:supervisor, @course]
     else
       render "edit"
     end
+  end
+
+  def show
+    @course = Course.find params[:id]
   end
 
   private
