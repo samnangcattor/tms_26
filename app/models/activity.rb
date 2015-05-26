342c55b6930a305a4cee2fa02576993f2a863cd5
DamSamNang <kcattor@gmail.com>
2015-05-26 13:24:01 +0700

    View activity course

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -4,4 +4,6 @@ class Activity < ActiveRecord::Base
   validates :user_id, presence: true
   validates :target_id, presence: true
   validates :state, presence: true, length: {maximum: 50}
+
+  scope :course_activities, ->(course_id){where target_id: course_id, state: "Joined"}
 end
