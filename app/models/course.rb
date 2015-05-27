c95178d73832a3c923a710f6fc70da629476278c
DamSamNang <kcattor@gmail.com>
2015-05-27 15:58:48 +0700

    Create Activity For Course

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,4 +1,6 @@
 class Course < ActiveRecord::Base
+  after_update :set_course_activity
+
   has_many :user_courses, dependent: :destroy
   has_many :users, through: :user_courses
   has_many :course_subjects, dependent: :destroy
@@ -7,4 +9,9 @@ class Course < ActiveRecord::Base
   validates :name, presence: true, length: {maximum: 50}
   validates :description, presence: true, length: {maximum: 500}
   validates :status, presence: true, length: {maximum: 10}
+
+  private 
+  def set_course_activity
+    users.each {|user| user.activities.create target_id: id, state: "Joined"}
+  end
 end
--- app/models/user_course.rb
+++ app/models/user_course.rb
@@ -4,4 +4,7 @@ class UserCourse < ActiveRecord::Base
   
   validates :user_id, presence: true
   validates :course_id, presence: true
+
+  scope :users_in_course, ->(user_id, course_id){where user_id: user_id,
+    course_id: course_id}
 end
