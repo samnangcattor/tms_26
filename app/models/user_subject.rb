07ec936125e89e0c722094d957fb10e88377bbab
DamSamNang <kcattor@gmail.com>
2015-06-15 13:29:04 +0700

    Create activities subject

--- app/models/user_subject.rb
+++ app/models/user_subject.rb
@@ -1,7 +1,14 @@
 class UserSubject < ActiveRecord::Base
+  after_create :set_subject_activity
+
   belongs_to :subject
   belongs_to :user
   
   validates :user_id, presence: true
   validates :subject_id, presence: true
+
+  private
+  def set_subject_activity
+    user.activities.create target_id: subject_id, state: Settings.learned
+  end
 end
