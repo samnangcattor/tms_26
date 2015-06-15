e522110054b82032251b2eb5dd84b64eef455366
DamSamNang <kcattor@gmail.com>
2015-06-15 13:29:04 +0700

    Create activity task

--- app/models/user_task.rb
+++ app/models/user_task.rb
@@ -1,7 +1,14 @@
 class UserTask < ActiveRecord::Base
+  after_create :set_task_activity
+
   belongs_to :user
   belongs_to :task
   
   validates :user_id, presence: true
   validates :task_id, presence: true
+
+  private
+  def set_task_activity
+    user.activities.create target_id: task_id, state: Settings.done
+  end
 end
